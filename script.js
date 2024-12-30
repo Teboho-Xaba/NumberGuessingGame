a463bcb3da05cdaa2c38bf64e495377c6159cf36
Tebza <99969842+Teboho-Xaba@users.noreply.github.com>
2024-12-30 20:03:42 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-document.querySelector('.message').textContent;
-document.querySelector('.number');
-document.querySelector('.score');
+// document.querySelector('.message').textContent;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(ocument.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
